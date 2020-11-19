a230a134f9c066f14f019545e7351473147c47ad
penlow99 <penlow99@yahoo.com>
2020-11-18 22:51:18 -0600

    updated

--- static/js/app.js
+++ static/js/app.js
@@ -20,3 +20,28 @@ function buildTable(data) {
     });
 }
 
+function handleClick() {
+    // we're telling D3 to look for the #datetime id in the HTML tags,
+    // By chaining .property("value"); to the d3.select function, we're telling D3 to look for where our 
+    // date values are stored on the webpage and grab that information and hold it in the "date" variable
+    let date = d3.select('#datetime').property('value');
+
+    // default filter will actually be the original table data
+    let filteredData = tableData;
+
+    // Check to see if a date was entered, then apply 'filter' method to table data
+    // only keeping the rows where the date matches the filter date
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+
+    // Rebuild the table using the buildTable function, with only the filtered rows
+    // !NOTE! >>> if no, date was entered, it will display all rows
+    buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
